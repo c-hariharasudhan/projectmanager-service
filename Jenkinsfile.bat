93317f5a26b30748e42b060a23f9d6f8ab0ea26a
Hariharasudhan Chandramurthy <hariharasudhan.chandramurthy@cognizant.com>
2019-06-25 19:41:05 +0530

    Jenkins file

---
+++ Jenkinsfile.bat
@@ -0,0 +1,36 @@
+ REM Path variables
+ SET FolderPath=%1
+ SET ResultsPath=C:\UnitTestResults
+ SET CoverageHistoryPath=C:\CoverageHistory
+ 
+ SET NunitPath=%FolderPath%\packages\NUnit.ConsoleRunner.3.10.0\tools
+ SET ReportUnitPath=%FolderPath%\packages\ReportUnit.1.2.1\tools
+ SET OpenCoverPath=%FolderPath%\packages\OpenCover.4.7.922\tools
+ SET ReportGeneratorPath=%FolderPath%\packages\ReportGenerator.2.5.10\tools
+ 
+ SET UnitTestProj=%FolderPath%\ProjectManager.Logic.Tests\ProjectManager.Logic.Tests.csproj
+ 
+ REM Recreate Results Folder
+ rd /S /Q %ResultsPath%
+ md %ResultsPath%
+ 
+ REM Create coverage history folder if not exists
+ if not exist "%CoverageHistoryPath%" mkdir %CoverageHistoryPath%
+ 
+ REM Run Nunit3 for tests, it produces TestResult.xml report
+ %NunitPath%\nunit3-console.exe %UnitTestProj% --result=%ResultsPath%\TestResult.xml
+ 
+ REM Get nunit command errorlevel
+ SET NunitError=%ERRORLEVEL%
+ 
+ REM Run ReportUnit to create HTML Report from Nunit XML report
+ %ReportUnitPath%\ReportUnit.exe %ResultsPath%\TestResult.xml
+ 
+ REM Run OpenCover to create coverage XML report
+ %OpenCoverPath%\OpenCover.Console.exe -register:user -register:user -target:%NunitPath%\nunit3-console.exe -targetargs:%UnitTestProj% -output:%ResultsPath%\opencovertests.xml
+ 
+ REM Run ReportGenerator to create coverage HTML report from coverage XML
+ %ReportGeneratorPath%\ReportGenerator.exe -reports:%ResultsPath%\opencovertests.xml -targetDir:%ResultsPath% -historydir:%CoverageHistoryPath%
+ 
+ REM Fail if Nunit has found an error on tests
+ exit /b %NunitError%
