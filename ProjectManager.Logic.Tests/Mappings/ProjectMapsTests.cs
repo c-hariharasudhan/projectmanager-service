1e86ff5eacf80ec6202064dbb9e75b99303de47d
Hariharasudhan Chandramurthy <hariharasudhan.chandramurthy@cognizant.com>
2019-06-26 17:51:03 +0530

    unit test cases

---
+++ ProjectManager.Logic.Tests/Mappings/ProjectMapsTests.cs
@@ -0,0 +1,72 @@
+﻿using AutoMapper;
+using NUnit.Framework;
+using ProjectManager.Logic.Tests.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Logic.Tests.Mappings
+{
+    [TestFixture]
+    public class ProjectMapsTests
+    {
+        private IMapper _mapper;
+        [SetUp]
+        public void Setup()
+        {
+            _mapper = UnityUlitity.SetupAutoMapper();
+        }
+        [Test]
+        public void ProjectMaps_EntityToBusinessObject()
+        {
+            var entity = new DataAccess.Entity.Project
+            {
+                Project_Id = 11,
+                Project_Name = "Project",
+                Start_Date = DateTime.Now,
+                End_Date = DateTime.Now.AddMonths(2),
+                Priority = 5
+            };
+            var result = _mapper.Map<BusinessObjects.Project>(entity);
+            Assert.That(result.ProjectId == 11);
+        }
+
+        [Test]
+        public void ProjectMaps_BusinessObjectToEntity()
+        {
+            var bo = new BusinessObjects.Project
+            {
+                ProjectName = "Test",
+                Priority = 10
+            };
+            var result = _mapper.Map<DataAccess.Entity.Project>(bo);
+            Assert.That(result.Priority == 10);
+        }
+
+        [Test]
+        public void ProjectMaps_MapsTasksCount()
+        {
+            var entity = new DataAccess.Entity.Project
+            {
+                Project_Id = 11,
+                Project_Name = "Project",
+                Start_Date = DateTime.Now,
+                End_Date = DateTime.Now.AddMonths(2),
+                Priority = 5,
+                Tasks = new List<DataAccess.Entity.Task> {
+                    new DataAccess.Entity.Task { Task_Id = 2, Task_Name = "Task 2", Project_Id = 11, Status = false},
+                    new DataAccess.Entity.Task { Task_Id = 3, Task_Name = "Task 3", Project_Id = 11, Status = true},
+                    new DataAccess.Entity.Task { Task_Id = 4, Task_Name = "Task 4", Project_Id = 11, Status = true},
+                }
+            };
+            var result = _mapper.Map<BusinessObjects.Project>(entity);
+            Assert.That(result.ProjectId == 11);
+            Assert.That(result.NoOfTasks == 3);
+            Assert.That(result.NoOfCompletedTasks == 1);
+
+        }
+
+    }
+}
---
+++ ProjectManager.Logic.Tests/Mappings/TaskMapsTests.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using NUnit.Framework;
+using ProjectManager.Logic.Tests.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Logic.Tests.Mappings
+{
+    [TestFixture]
+    public class TaskMapsTests
+    {
+        private IMapper _mapper;
+        [SetUp]
+        public void Setup()
+        {
+            _mapper = UnityUlitity.SetupAutoMapper();
+        }
+        [Test]
+        public void TaskMaps_EntityToBusinessObject()
+        {
+            var entity = new DataAccess.Entity.Task
+            {
+                Task_Id = 111,
+                Task_Name = "Task",
+                Parent_Id = 1,
+                Status = true
+            };
+            var result = _mapper.Map<BusinessObjects.Task>(entity);
+            Assert.That(result.TaskId == 111);
+        }
+
+        [Test]
+        public void UserMaps_BusinessObjectToEntity()
+        {
+            var bo = new BusinessObjects.Task
+            {
+                TaskId = 111,
+                TaskName = "test",
+                Priority = 10,
+                ProjectId = 1,
+                ParentId = 1
+            };
+            var result = _mapper.Map<DataAccess.Entity.Task>(bo);
+            Assert.That(result.Project_Id == 1);
+            Assert.That(result.Parent_Id == 1);
+            Assert.That(result.Task_Id == 111);
+        }
+    }
+}
